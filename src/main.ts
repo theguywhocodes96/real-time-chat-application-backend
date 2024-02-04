06708d26ba896747122907c6d5f0d611812658d1
theguywhocodes96 <haidermalik1996@gmail.com>
2024-02-04 18:36:09 +0500

    - swagger implemented - prettier setup with settings.json file

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors();
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Real time chat application')
+    .setDescription('REAL TIME CHAT APPLICATION REST APIs')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT);
+
+  console.warn(`App is listening on port: ${process.env.PORT}`);
 }
 bootstrap();
